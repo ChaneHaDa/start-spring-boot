9656c1ee34b4305d577b1ac0d890cb3ce1763b22
ChaneHaDa <chane.ha.da@gmail.com>
2024-03-13 16:39:44 +0900

    add CRUD

--- src/main/java/com/chan/ssb/TeamController.java
+++ src/main/java/com/chan/ssb/TeamController.java
@@ -1,9 +1,8 @@
 package com.chan.ssb;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -12,13 +11,44 @@ public class TeamController {
 
     // Team: id, name, city, championships
     private final AtomicLong counter = new AtomicLong();
+    private final List<Team> teams = new ArrayList<>();
 
-    @RequestMapping("/team")
-    public List<Team> retrieveAllTeams() {
-        return Arrays.asList(
-                new Team(counter.incrementAndGet(), "Lakers", "Los Angeles", 17),
-                new Team(counter.incrementAndGet(), "Celtics", "Boston", 17),
-                new Team(counter.incrementAndGet(), "Warriors", "Golden State", 6)
-        );
+    @GetMapping("/team")
+    public List<Team> getAllTeams() {
+        return teams;
+    }
+
+    @GetMapping("/team/{id}")
+    public Team getTeam(@PathVariable long id) {
+        for (Team team : teams) {
+            if (team.id() == id) {
+                return team;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("/team")
+    public Team createTeam(Team team) {
+        Team newTeam = new Team(counter.incrementAndGet(), team.name(), team.city(), team.championships());
+        teams.add(newTeam);
+        return newTeam;
+    }
+
+    @PutMapping("/team/{id}")
+    public Team updateTeam(@PathVariable long id, Team team) {
+        for (int i = 0; i < teams.size(); i++) {
+            if (teams.get(i).id() == id) {
+                Team updatedTeam = new Team(id, team.name(), team.city(), team.championships());
+                teams.set(i, updatedTeam);
+                return team;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/team/{id}")
+    public void deleteTeam(@PathVariable long id) {
+        teams.removeIf(team -> team.id() == id);
     }
 }
