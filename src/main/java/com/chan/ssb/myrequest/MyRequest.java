e592d99c406d7cd1919264243581b11ade1afc4d
ChaneHaDa <chane.ha.da@gmail.com>
2024-03-19 16:45:19 +0900

    request

---
+++ src/main/java/com/chan/ssb/myrequest/MyRequest.java
@@ -0,0 +1,41 @@
+package com.chan.ssb.myrequest;
+
+public class MyRequest {
+    private long id;
+    private String name;
+    private String city;
+
+    public MyRequest() {
+    }
+
+    public MyRequest(long id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
