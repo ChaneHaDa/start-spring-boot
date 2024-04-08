8706754d5b0600526ebeb338f21dcaa879aa278f
ChaneHaDa <chane.ha.da@gmail.com>
2024-04-08 13:58:08 +0900

    access

--- src/main/java/com/chan/ssb/security/SpringSecurityConfiguration.java
+++ src/main/java/com/chan/ssb/security/SpringSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.chan.ssb.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,10 +12,20 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SpringSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                auth -> auth.anyRequest().authenticated()
-        );
-        http.httpBasic(withDefaults());
+//        http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults());
+
+//        http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/players").authenticated()
+                        .requestMatchers("/api/v1/team").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
         http.csrf().disable();
         return http.build();
     }
