17dc2d9276c804eaa55a022d14c84bc565755fc5
ChaneHaDa <chane.ha.da@gmail.com>
2024-04-16 16:51:41 +0900

    csrf

--- src/main/java/com/chan/ssb/security/SpringSecurityConfiguration.java
+++ src/main/java/com/chan/ssb/security/SpringSecurityConfiguration.java
@@ -1,13 +1,18 @@
 package com.chan.ssb.security;
 
+import com.chan.ssb.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -18,13 +23,12 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
-//                .formLogin(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults());
 
-//        http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
-//                .formLogin(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults());
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+        http.securityContext((context) -> context.requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
 
         http.cors().configurationSource(new CorsConfigurationSource() {
             @Override
@@ -38,48 +42,29 @@ public class SpringSecurityConfiguration {
                 config.setMaxAge(1800L);
                 return config;
             }
-        }).and().csrf().disable();
+        });
+
+        http.csrf((csrf)-> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/user/**", "/h2-console/**")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
+
 
         http.authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/api/players", "/h2-console/**").authenticated()
-                        .requestMatchers("/api-docs","/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/user", "/api/v1/team").permitAll()
+                        .requestMatchers("/api/**", "/h2-console/**").authenticated()
+                        .requestMatchers("/api-docs","/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/user/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
         http.headers().frameOptions().sameOrigin();
 
-        http.csrf().disable();
         return http.build();
     }
 
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//        UserDetails admin = User.withUsername("admin")
-//                .username("admin")
-//                .password("12345")
-//                .authorities("admin")
-//                .build();
-//        UserDetails user = User.withUsername("user")
-//                .password("12345")
-//                .authorities("read")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-//        return NoOpPasswordEncoder.getInstance();
-//        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_5();
-//        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-//        return SCryptPasswordEncoder.defaultsForSpringSecurity_v4_1();
-//        return SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8();
-//        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_2();
-//        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-//        return new StandardPasswordEncoder();
-
     }
 
 }
