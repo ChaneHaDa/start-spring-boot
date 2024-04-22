5cac26ef7c5952d7a12bb968c46107b65c8972f6
ChaneHaDa <chane.ha.da@gmail.com>
2024-04-22 12:14:23 +0900

    oauth2 client

--- src/main/java/com/chan/ssb/config/SpringSecurityConfiguration.java
+++ src/main/java/com/chan/ssb/config/SpringSecurityConfiguration.java
@@ -24,49 +24,49 @@ import java.util.List;
 @Configuration
 public class SpringSecurityConfiguration {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-        requestHandler.setCsrfRequestAttributeName("_csrf");
-
-        http.sessionManagement((sessionManagement) ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
-
-        http.cors(cors -> cors.configurationSource(request -> {
-            CorsConfiguration config = new CorsConfiguration();
-            config.setAllowedOrigins(List.of("http://localhost:3000"));
-            config.setAllowedMethods(List.of("GET", "POST"));
-            config.setAllowCredentials(true);
-            config.setAllowedHeaders(List.of("*"));
-            config.setExposedHeaders(List.of("Authorization"));
-            config.setMaxAge(1800L);
-            return config;
-        }));
-
-        http.csrf(csrf-> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/user/**", "/authority", "/h2-console/**")
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-            .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-            .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-            .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class);
-
-
-
-        http.authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/authority").hasRole("ADMIN")
-                        .requestMatchers("/api/**").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/user/**", "/api-docs","/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/h2-console/**").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-
-        http.headers(headers -> headers.frameOptions(
-                HeadersConfigurer.FrameOptionsConfig::sameOrigin
-        ));
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+//        requestHandler.setCsrfRequestAttributeName("_csrf");
+//
+//        http.sessionManagement((sessionManagement) ->
+//                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        );
+//
+//        http.cors(cors -> cors.configurationSource(request -> {
+//            CorsConfiguration config = new CorsConfiguration();
+//            config.setAllowedOrigins(List.of("http://localhost:3000"));
+//            config.setAllowedMethods(List.of("GET", "POST"));
+//            config.setAllowCredentials(true);
+//            config.setAllowedHeaders(List.of("*"));
+//            config.setExposedHeaders(List.of("Authorization"));
+//            config.setMaxAge(1800L);
+//            return config;
+//        }));
+//
+//        http.csrf(csrf-> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/user/**", "/authority", "/h2-console/**")
+//            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+//            .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+//            .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+//            .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class);
+//
+//
+//
+//        http.authorizeHttpRequests(requests -> requests
+//                        .requestMatchers("/authority").hasRole("ADMIN")
+//                        .requestMatchers("/api/**").hasAnyRole("USER", "ADMIN")
+//                        .requestMatchers("/user/**", "/api-docs","/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/h2-console/**").permitAll()
+//                        .anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults());
+//
+//        http.headers(headers -> headers.frameOptions(
+//                HeadersConfigurer.FrameOptionsConfig::sameOrigin
+//        ));
+//
+//        return http.build();
+//    }
 
 
     @Bean
